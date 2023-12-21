380ee012bf67a18feafe29f34025949dcac8a5e2
DabneyLI <144627567+DabneyLI@users.noreply.github.com>
2023-12-21 11:59:35 +0100

    Update whois.js

--- my-app-next-js/pages/api/whois.js
+++ my-app-next-js/pages/api/whois.js
@@ -2,94 +2,89 @@ import { supabase } from "../../utils/supabaseClient";
 
 export default async function handler(req, res) {
   const { query } = req.query;
-  // Check if query is not null and contains a period
-  if (!query || !query.includes(".")) {
-    return res.status(400).json({ error: "Invalid domain format" });
+  if (!query) {
+    return res.status(400).json({ error: "Domain query is required" });
   }
-  const [name, suffix] = query.split(".");
 
-  try {
-    let { data: records, error } = await supabase
-      .from("queries")
-      .select("*")
-      .eq("domain", query);
+  // 拆分输入的域名和后缀
+  let [name, ...inputSuffix] = query.split(".");
+  inputSuffix = inputSuffix.join("."); // 处理可能的多级后缀
 
-    if (error) {
-      console.error("Error fetching from Supabase:", error);
-      return res.status(500).json({ error: error.message });
-    }
+  // 定义所有支持的后缀
+  const allSuffixes = [
+    "com",
+    "net",
+    "org",
+    "me",
+    "xyz",
+    "info",
+    "io",
+    "co",
+    "ai",
+    "biz",
+    "us",
+  ];
+  // 如果输入了后缀，确保它是第一个被查询的
+  if (inputSuffix) {
+    allSuffixes.unshift(inputSuffix);
+  }
 
-    const recentRecord = records.find(
-      (record) => new Date() - new Date(record.timestamp) < 86400000, // 24 hours in milliseconds
-    );
+  const domainResults = [];
 
-    if (recentRecord) {
-      const updatedTimestamp = new Date().toISOString();
-      const { error: updateError } = await supabase
+  // 对每个后缀进行查询
+  for (const suffix of allSuffixes) {
+    const domain = `${name}.${suffix}`;
+    try {
+      let { data: records, error: fetchError } = await supabase
         .from("queries")
-        .update({ timestamp: updatedTimestamp })
-        .match({ id: recentRecord.id });
+        .select("*")
+        .eq("domain", domain);
 
-      if (updateError) {
-        console.error("Error updating timestamp in Supabase:", updateError);
-        throw updateError;
-      }
+      if (fetchError) throw fetchError;
 
-      return res.status(200).json(recentRecord);
-    } else {
-      const apiUrl = `https://whois.freeaiapi.xyz/?name=${name}&suffix=${suffix}`;
-      const response = await fetch(apiUrl);
+      let recentRecord = records.find(
+        (r) => new Date() - new Date(r.timestamp) < 86400000,
+      );
+      if (recentRecord) {
+        // 更新时间戳
+        const { error: updateError } = await supabase
+          .from("queries")
+          .update({ timestamp: new Date().toISOString() })
+          .match({ id: recentRecord.id });
 
-      if (!response.ok) {
-        // If the WHOIS API fails to fetch the data, do not save to database and return an error message
-        return res
-          .status(200)
-          .json({ error: "Error for domain search, not supported." });
-      }
-
-      const whoisData = await response.json();
-
-      // Check if the status information was successfully retrieved and domain is supported
-      if (whoisData.status !== "ok") {
-        // If the domain is not supported or another error occurred, do not save to database
-        return res
-          .status(200)
-          .json({ error: "Error for domain search, not supported." });
-      }
-
-      // Extract the required details from whoisData
-      let newRecord;
-      // Check if the domain is registered
-      if (!whoisData.available) {
-        newRecord = {
-          domain: whoisData.domain, // domain name
-          registered: whoisData.available === false, // registration status
-          creationDate: whoisData.creation_datetime, // domain creation date
-          expiryDate: whoisData.expiry_datetime, // domain expiry date
-
-          timestamp: new Date().toISOString(), // current timestamp
-        };
+        if (updateError) throw updateError;
+        domainResults.push(recentRecord);
       } else {
-        newRecord = {
-          domain: whoisData.domain, // domain name
-          registered: whoisData.available === false, // registration status
-          timestamp: new Date().toISOString(), // current timestamp
+        // 执行新的 WHOIS 查询
+        const response = await fetch(
+          `https://whois.freeaiapi.xyz/?name=${name}&suffix=${suffix}`,
+        );
+        if (!response.ok) continue;
+
+        const whoisData = await response.json();
+        if (whoisData.status !== "ok") continue;
+
+        let newRecord = {
+          domain,
+          registered: whoisData.available,
+          timestamp: new Date().toISOString(),
+          // 这里添加了从响应中获取的其他WHOIS信息
         };
+
+        const { error: insertError } = await supabase
+          .from("queries")
+          .insert([newRecord]);
+
+        if (insertError) throw insertError;
+        domainResults.push(newRecord);
       }
-
-      const { error: insertError } = await supabase
-        .from("queries")
-        .insert([newRecord]);
-
-      if (insertError) {
-        console.error("Error saving to Supabase:", insertError);
-        return res.status(500).json({ error: insertError.message });
-      }
-
-      res.status(200).json(newRecord);
+    } catch (error) {
+      // 记录错误但继续处理
+      console.error(`Error with domain ${domain}:`, error.message);
+      domainResults.push({ domain, error: error.message });
     }
-  } catch (error) {
-    console.error("API route error:", error);
-    res.status(500).json({ error: error.message });
   }
+
+  // 返回所有查询的结果
+  return res.status(200).json(domainResults);
 }
